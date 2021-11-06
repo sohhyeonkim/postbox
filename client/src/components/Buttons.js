06bcb1d56a3968cbc3f797cf69c2aa9199730d34
sohhyeonkim <sohhyeonkim@gmail.com>
2021-11-06 14:05:37 +0900

    Complete Buttons.js mockup

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,8 +1,33 @@
-function Buttons() {
+import styled from "styled-components";
+
+function Buttons({ props }) {
   return (
     <>
-      <h1>Buttons</h1>
+      <StyledDiv>
+        <StyledButton>임시저장</StyledButton>
+        <StyledButton send>전송하기</StyledButton>
+      </StyledDiv>
     </>
   );
 }
+const StyledDiv = styled.div`
+  width: 240px;
+  height: 36px;
+  border: 1px solid red;
+  margin: 1em auto;
+`;
+const StyledButton = styled.button`
+  background: ${(props) => (props.send ? "palevioletred" : "white")};
+  color: ${(props) => (props.send ? "white" : "palevioletred")};
+  float: ${(props) => (props.send ? "right" : "left")};
+  &:hover {
+    cursor: pointer;
+  }
+  font-size: 1em;
+  /* margin: 1em; */
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 export default Buttons;
